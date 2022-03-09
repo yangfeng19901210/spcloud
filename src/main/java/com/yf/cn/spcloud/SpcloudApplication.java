626ff5fd289a1871a33ec8e508b1a44f31036789
“yangfeng” <18334723118@163.com>
2022-03-09 10:36:08 +0800

    修改启动类提交

--- src/main/java/com/yf/cn/spcloud/SpcloudApplication.java
+++ src/main/java/com/yf/cn/spcloud/SpcloudApplication.java
@@ -2,7 +2,9 @@ package com.yf.cn.spcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/***
+ *spring启动类
+ **/
 @SpringBootApplication
 public class SpcloudApplication {
 
