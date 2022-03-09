926273c3a070bf4b7fc7069bc69ac952838501f3
“yangfeng” <18334723118@163.com>
2022-03-09 10:25:28 +0800

    Initial commit

---
+++ src/main/java/com/yf/cn/spcloud/SpcloudApplication.java
@@ -0,0 +1,13 @@
+package com.yf.cn.spcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpcloudApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpcloudApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/yf/cn/spcloud/SpcloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yf.cn.spcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpcloudApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
